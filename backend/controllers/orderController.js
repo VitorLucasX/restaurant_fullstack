50137c084f420430c47b9683671c09f8fc472f63
Vitor Lucas Araujo <126624364+VitorLucasX@users.noreply.github.com>
2024-07-01 21:51:22 -0300

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-     const frontend_url = "http://localhost:5173";
+     const frontend_url = "https://restaurant-frontend-86e5.onrender.com/";
 
      try {
           const newOrder = new orderModel({
